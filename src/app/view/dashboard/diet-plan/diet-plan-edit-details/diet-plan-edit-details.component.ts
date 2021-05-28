38b131b6427fa071e86d84ad5896435b41fccb4d
DevMedo <idevmedo@gmail.com>
2021-05-28 20:50:53 +0300

    diet plan edit view changes

--- src/app/view/dashboard/diet-plan/diet-plan-edit-details/diet-plan-edit-details.component.ts
+++ src/app/view/dashboard/diet-plan/diet-plan-edit-details/diet-plan-edit-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DietPlanService } from './../../../../_services/diet-plan.service';
 
 @Component({
   selector: 'app-diet-plan-edit-details',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diet-plan-edit-details.component.scss']
 })
 export class DietPlanEditDetailsComponent implements OnInit {
-
-  constructor() { }
+  calories : number = 0;
+  protein : number = 0;
+  carbs : number = 0;
+  fat : number = 0;
+  constructor(private dietplan_service:DietPlanService) {
+    this.calories = dietplan_service.dietPlanOjbect.calories;
+    this.protein = dietplan_service.dietPlanOjbect.protein;
+    this.carbs = dietplan_service.dietPlanOjbect.carbs;
+    this.fat = dietplan_service.dietPlanOjbect.fat;
+  }
 
   ngOnInit(): void {
   }
