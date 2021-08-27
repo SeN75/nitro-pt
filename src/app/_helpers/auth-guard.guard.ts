e33f2af8b2e5bacf106194ba1f9d8d3a33b3f9c0
saleh alsaggaf <saa111954@hotmail.com>
2021-08-28 02:26:45 +0300

    edit canActive

--- src/app/_helpers/auth-guard.guard.ts
+++ src/app/_helpers/auth-guard.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { IdentityService } from '../_services/identity/identity.service';
+import { LoggerService } from '../_services/logger.service';
 
 let token: any = localStorage.getItem('refreshToken')
 let authToken: any = localStorage.getItem('authToken')
@@ -10,26 +11,39 @@ let authToken: any = localStorage.getItem('authToken')
 })
 
 export class AuhtGuardGuard implements CanActivate {
-  constructor(private identitySrv: IdentityService, private router: Router) { }
+  constructor(private identitySrv: IdentityService, private router: Router, private logger: LoggerService) { }
   canActivate(next: ActivatedRouteSnapshot): boolean {
-    return true;
-    // if (token) {
-    //   this.identitySrv._refreshToken({ refresh: token }).subscribe(auth => {
-    //     if (auth) {
-    //       localStorage.setItem('refreshToken', auth + "")
-    //       return true
-    //     }
-    //     else {
-    //       localStorage.clear()
-    //       this.router.navigateByUrl("/register/login")
-    //       return false
-    //     }
-    //   }, error => this.router.navigateByUrl("/register/login"))
-    //   return true
-    // } else {
-    //   this.router.navigateByUrl("/register/login")
 
-    //   return false
-    // }
+    // this.logger.log("d")
+    // return true;
+    if (token) {
+      this.identitySrv._refreshToken({ refresh: token }).subscribe((auth: any) => {
+        if (auth) {
+          this.logger.log('auth', auth)
+          localStorage.setItem('refreshToken', auth.refresh + "")
+          localStorage.setItem('authToken', auth.access + "")
+          return true
+        }
+        else {
+          localStorage.clear()
+          localStorage.removeItem('refreshToken')
+          localStorage.removeItem('authToken')
+          this.router.navigateByUrl("/register/login")
+          return false
+        }
+      }, error => {
+        localStorage.removeItem('refreshToken')
+        localStorage.removeItem('authToken')
+        this.router.navigateByUrl("/register/login")
+      })
+      return true
+    } else {
+      localStorage.clear()
+      localStorage.removeItem('refreshToken')
+      localStorage.removeItem('authToken')
+      this.router.navigateByUrl("/register/login")
+
+      return false
+    }
   }
 }
