828725619c2ffeb2219290046ccde27e67f7c159
saleh alsaggaf <saa111954@hotmail.com>
2021-10-19 22:33:43 +0300

    translate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { LanguageService } from './_services/language.service';
+import { IdentityService } from 'src/app/_services/identity/identity.service';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,13 @@ import { LanguageService } from './_services/language.service';
 })
 export class AppComponent {
   title = 'nitro-pt';
-  constructor(private lang: LanguageService, private cookieSrv: CookieService) {
+  constructor(private lang: LanguageService, private cookieSrv: CookieService, private identitySrv: IdentityService) {
     if (!cookieSrv.get('loggedin') && localStorage.getItem('refreshToken')) {
       cookieSrv.delete('loggedin')
       localStorage.removeItem('refreshToken')
       localStorage.removeItem('authToken')
+    } else {
+      this.identitySrv.getUserProfileByJWT()
     }
   }
 }
