b2e07c862fbde802175b4498bce4d51ffda7e084
saleh alsaggaf <saa111954@hotmail.com>
2021-11-13 15:59:58 +0300

    create page food category, add category, edit category and delete category

--- src/app/view/dashboard/categories/category-type-dialog/category-type-dialog.component.ts
+++ src/app/view/dashboard/categories/category-type-dialog/category-type-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogService } from 'src/app/_services/dialog.service';
+import { FoodItemsService } from 'src/app/_services/dite/food-items.service';
 import { FoodCategoriesService } from './../../../../_services/dite/food-categories.service';
 
 @Component({
@@ -10,13 +12,16 @@ import { FoodCategoriesService } from './../../../../_services/dite/food-categor
 })
 export class CategoryTypeDialogComponent implements OnInit {
   categoryForm: FormGroup | any;
+  isSend = false;
   constructor(public dialogRef: MatDialogRef<CategoryTypeDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogSrv: DialogService,
     private categorySrv: FoodCategoriesService,
+    private categoryItemSrv: FoodItemsService,
     private formBuilder: FormBuilder) {
     this.categoryForm = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.pattern("[A-Za-z ]+")]],
-      name_ar: ['', [Validators.required, Validators.pattern("^[\u0621-\u064A\u0660-\u0669 ]+$")]],
+      name: ['', [Validators.required, Validators.pattern("[A-Za-z0-9 ]+")]],
+      name_ar: ['', [Validators.required, Validators.pattern("^[\u0621-\u064A\u0660-\u0669-0-9 ]+$")]],
     })
   }
   ngOnInit(): void {
@@ -26,11 +31,21 @@ export class CategoryTypeDialogComponent implements OnInit {
     }
   }
   delete() {
-    this.categorySrv.deleteFoodCategoriesById(this.data.cate.id);
-    this.onNoClick();
+
+    if (this.categoryItemSrv.getItemsWithSameCategory(this.data.cate.name).length > 0)
+      this.dialogSrv.deleteDialog({ name: 'food category2', id: this.data.cate.id, dialog: this.dialogSrv })
+    else
+      this.dialogSrv.deleteDialog({ name: 'food category1', id: this.data.cate.id, dialog: this.dialogSrv })
+
   }
   action() {
+    this.isSend = true;
+    let data = { ...this.categoryForm.value }
     if (this.data.state == 'edit') {
+      if (!this.categoryForm.get('name')?.dirty)
+        delete data.name
+      if (!this.categoryForm.get('name_ar')?.dirty)
+        delete data.name_ar
       this.categorySrv.updateFoodCategoriesById(this.categoryForm.value, this.data.cate.id)
       this.onNoClick()
     } else {
