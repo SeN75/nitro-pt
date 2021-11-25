40df416cabed97a6645989445569c9774dd320fb
saleh alsaggaf <saa111954@hotmail.com>
2021-11-26 02:20:46 +0300

    Create Refresh Token with Interceptor and JWT

---
+++ src/app/_services/identity/token-storage.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+const TOKEN_KEY = 'auth-token';
+const REFRESHTOKEN_KEY = 'auth-refreshtoken';
+const USER_KEY = 'auth-user';
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenStorageService {
+  constructor() { }
+
+  signOut(): void {
+    window.sessionStorage.clear();
+  }
+
+  public saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+
+    const user = this.getUser();
+    if (user.external_id) {
+      this.saveUser({ ...user, accessToken: token });
+    }
+  }
+
+  public getToken(): string | null {
+    return window.sessionStorage.getItem(TOKEN_KEY);
+  }
+
+  public saveRefreshToken(token: string): void {
+    window.sessionStorage.removeItem(REFRESHTOKEN_KEY);
+    window.sessionStorage.setItem(REFRESHTOKEN_KEY, token);
+  }
+
+  public getRefreshToken(): string | null {
+    return window.sessionStorage.getItem(REFRESHTOKEN_KEY);
+  }
+
+  public saveUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+  public getUser(): any {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    return {
+      first_name: 'not data',
+      first_name_ar: 'لايوجد',
+      middle_name: '',
+      middle_name_ar: '',
+      last_name: '',
+      last_name_ar: '',
+      email: 'لايوجد',
+      phone_number: 'لايوجد',
+      display_name_ar: 'not found',
+      groups: [0]
+    };
+  }
+}
